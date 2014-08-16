9e89aa03713602600c86ffce62b7e80f77f446f1
ppatarinski <paulpatarinski@gmail.com>
2014-08-16 18:56:06 -0500

    Initial commit

---
+++ Core/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Core
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new MainPage ();
+		}
+	}
+}
+
---
+++ Core/Helpers/HttpClientExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Core.Helpers
+{
+	public static class HttpClientExtensions
+	{
+		public static async Task<T> GetAsync<T> (this HttpClient client, string url)
+		{
+			var httpRequest = new HttpRequestMessage (new HttpMethod ("GET"), url);
+
+			client.Timeout = TimeSpan.FromSeconds (60);
+
+			var response = await client.SendAsync (httpRequest);
+
+			var jsonString = await response.Content.ReadAsStringAsync ();
+
+			var result = JsonConvert.DeserializeObject<T> (jsonString);
+
+			return result;
+		}
+
+		public static async Task<string> GetStringAsync (this HttpClient client, string url)
+		{
+			var httpRequest = new HttpRequestMessage (new HttpMethod ("GET"), url);
+
+			var response = client.SendAsync (httpRequest).Result;
+
+			var jsonString = await response.Content.ReadAsStringAsync ();
+
+			return jsonString;
+		}
+	}
+}
