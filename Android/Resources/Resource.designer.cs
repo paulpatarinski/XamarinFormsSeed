3793406c3517817d01eca7136c06bc6627c12e80
ppatarinski <paulpatarinski@gmail.com>
2014-08-25 22:18:51 -0500

    Changing the default icons

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -240,11 +240,11 @@ namespace ThatConfXamarin.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040002
-			public const int ApplicationName = 2130968578;
-			
 			// aapt resource value: 0x7f040001
-			public const int Hello = 2130968577;
+			public const int ApplicationName = 2130968577;
+			
+			// aapt resource value: 0x7f040000
+			public const int Hello = 2130968576;
 			
 			// aapt resource value: 0x7f040004
 			public const int app_name = 2130968580;
@@ -252,8 +252,8 @@ namespace ThatConfXamarin.Android
 			// aapt resource value: 0x7f040003
 			public const int hello = 2130968579;
 			
-			// aapt resource value: 0x7f040000
-			public const int library_name = 2130968576;
+			// aapt resource value: 0x7f040002
+			public const int library_name = 2130968578;
 			
 			static String()
 			{
